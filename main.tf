b1768b95edbd2ea683703f70c1872ec31e944c48
Charitha Ramala <92932173+RamalaCharitha@users.noreply.github.com>
2026-06-29 19:57:10 +0530

    Change EC2 instance type from t3.micro to c7i-flex.large

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # 2. Create the EC2 Instance
 resource "aws_instance" "simple_ec2" {
   ami           = "ami-08f44e8eca9095668" # Standard Ubuntu 24.04 AMI in us-east-1
-  instance_type = "t3.micro"
+  instance_type = "c7i-flex.large"
 
   tags = {
     Name = "Harness-IaCM-Demo"
